f8170164a09827a4b074c722444ba1424ecf6e15
Lemucya <lemucya@mail.ru>
2022-09-18 20:40:09 +0700

    Написали программу, для решения задачи6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ля число четным (делится ли оно на два без остатка)
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 1)
+{
+    Console.WriteLine("Нет, число " + number + " является нечетным");
+}
+else
+{
+    Console.WriteLine("Да, число " + number + " является четным");
+}
